57e28101e64e9940cb61115f9d1859826974f3a9
fernandoavanzo <fernando.avanzo@gmail.com>
2024-08-10 12:55:38 -0300

    improve install config

--- rclone/install.sh
+++ rclone/install.sh
@@ -4,8 +4,10 @@ export DEV_RCLONE="$HOME/Projects/helpers-bash-scripts/rclone"
 #prod
 export RCLONE="$HOME/.Library/rclone"
 
-cp -f "$DEV_RCLONE"/init_rclone.sh "$DEV_RCLONE"
+cp -f "$DEV_RCLONE"/init_rclone.sh "$RCLONE"
 cp -rf "$DEV_RCLONE"/bin "$RCLONE"
 
 ls -al "$RCLONE"
 ls -al "$RCLONE"/bin
+
+cat "$RCLONE"/init_rclone.sh
