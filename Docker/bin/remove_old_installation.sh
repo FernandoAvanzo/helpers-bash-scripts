fa39015cffc1c2c9fdfd606ec9d872cebb93ddb3
fernandoavanzo <fernando.avanzo@gmail.com>
2024-08-11 15:00:11 -0300

    Script ro remove old docker installations

---
+++ Docker/bin/remove_old_installation.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+remove_old_docker() {
+  /usr/bin/expect <<EOF
+set timeout -1
+
+spawn sudo apt purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+expect "password for"
+
+send -- "fer010486\r"
+expect eof
+EOF
+
+  if [ -d /var/lib/docker ]; then
+    sudo rm -rf /var/lib/docker
+  fi
+
+  if [ -d /var/lib/containerd ]; then
+    sudo rm -rf /var/lib/containerd
+  fi
+}
+
+remove_conflict_packages() {
+  local pkg
+  for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
+    if dpkg-query -W -f='${Status}' $pkg 2>/dev/null | grep -q "install ok installed"; then
+      /usr/bin/expect <<EOF
+set timeout -1
+
+spawn sudo apt-get remove -y $pkg
+expect "password for"
+
+send -- "fer010486\r"
+expect eof
+EOF
+    else
+      echo "$pkg is not installed."
+    fi
+  done
+}
+
+export -f remove_old_docker
+export -f remove_conflict_packages
+
+remove_old_docker
+remove_conflict_packages
