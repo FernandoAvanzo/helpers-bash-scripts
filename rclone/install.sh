afe3f0af4ab34fdac303c346fab4d8a559189dbd
fernandoavanzo <fernando.avanzo@gmail.com>
2024-08-11 08:31:31 -0300

    improve install config

--- rclone/install.sh
+++ rclone/install.sh
@@ -4,9 +4,13 @@ export DEV_RCLONE="$HOME/Projects/helpers-bash-scripts/rclone"
 #prod
 export RCLONE="$HOME/.Library/rclone"
 
+sudo ln -sf "$HOME"/.Library /root/.Library
+
 cp -f "$DEV_RCLONE"/rclone-mount.service "$RCLONE"
 cp -f "$DEV_RCLONE"/init_rclone.sh "$RCLONE"
 cp -rf "$DEV_RCLONE"/bin "$RCLONE"
 
 ls -al "$RCLONE"
 ls -al "$RCLONE"/bin
+
+sudo ls -al /root/.Library
