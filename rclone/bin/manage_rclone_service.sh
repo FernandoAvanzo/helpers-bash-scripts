84358d570bd6fcc66495eaaf88c5db8db6d43727
fernandoavanzo <fernando.avanzo@gmail.com>
2024-08-18 10:11:43 -0300

    improve rclone init script

--- rclone/bin/manage_rclone_service.sh
+++ rclone/bin/manage_rclone_service.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 manage_rclone_service() {
+  local password
+  password="fer010486"
+
   # Reload the systemd manager configuration
-  if ! systemctl daemon-reload; then
+  if ! echo $password | sudo -S systemctl daemon-reload; then
     echo "Failed to reload the systemd daemon."
     return 1
   fi
 
   # Enable the rclone service
-  if ! systemctl enable rclone-mount.service; then
+  if ! echo $password | sudo -S systemctl enable rclone-mount.service; then
     echo "Failed to enable rclone-mount.service."
     return 1
   fi
 
   # Start the rclone service
-  if ! systemctl start rclone-mount.service; then
+  if ! echo $password | sudo -S systemctl start rclone-mount.service; then
     echo "Failed to start rclone-mount.service."
     return 1
   fi
 
   echo "rclone-mount.service successfully reloaded, enabled, and started."
   return 0
-}
+  }
