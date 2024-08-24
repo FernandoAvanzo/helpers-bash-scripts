bb8530c47952d92650cb9d421c2f133f8feca2b3
FernandoAvanzo <fernando.avanzo@gmail.com>
2024-08-23 21:35:14 -0300

    Improve the runner

--- PulaMariana/pula_mariana.sh
+++ PulaMariana/pula_mariana.sh
@@ -12,9 +12,9 @@ export JAVA_AGENT="$IDEA_RT:$ULTIMATE_BIN"
 export LOCAL_CLASSPATH="$MARIANA/build/classes/kotlin/main"
 export KOTLIN_STD_LIB="$GRADLE_JETBRAINS_MODULES/org.jetbrains.kotlin/kotlin-stdlib/2.0.20/7388d355f7cceb002cd387ccb7ab3850e4e0a07f/kotlin-stdlib-2.0.20.jar"
 export ANNOTATIONS="$GRADLE_JETBRAINS_MODULES/org.jetbrains/annotations/13.0/919f0dfe192fb4e063e7dacadee7f8bb9a2672a9/annotations-13.0.jar"
-export COMMAND_RUN="-javaagent:${JAVA_AGENT} -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath ${LOCAL_CLASSPATH}:${KOTLIN_STD_LIB}:${ANNOTATIONS} MainKt"
+export MARIANA_JUMP="-javaagent:${JAVA_AGENT} -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath ${LOCAL_CLASSPATH}:${KOTLIN_STD_LIB}:${ANNOTATIONS} MainKt"
 
 
 "$BASH_SCRIPTS"/gradlew clean
 "$BASH_SCRIPTS"/gradlew build
-$JAVA_21 $COMMAND_RUN
+$JAVA_21 $MARIANA_JUMP
