0c4987a5530e3ae8530959fc533262094bb5c408
FernandoAvanzo <fernando.avanzo@gmail.com>
2024-08-24 12:16:10 -0300

    improve the docker remove script

--- Docker/bin/install_docker_engine.sh
+++ Docker/bin/install_docker_engine.sh
@@ -2,9 +2,9 @@
 
 sudo_password="fer010486"
 
-add_docker_apt_keyrings () {
+add_docker_apt_keyrings() {
   # The command block to be executed by expect
-  expect << EOF
+  expect <<EOF
   set timeout -1
   spawn /bin/bash -c "sudo apt update && sudo apt install -y ca-certificates curl && sudo install -m 0755 -d /etc/apt/keyrings && sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && sudo chmod a+r /etc/apt/keyrings/docker.asc"
 
@@ -15,40 +15,40 @@ add_docker_apt_keyrings () {
 EOF
 }
 
-add_docker_repository () {
-  # The command block to be executed by expect
-  expect << EOF
-  set timeout -1
-  spawn /bin/bash -c "echo \
-    \"deb [arch=\$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    \$(. /etc/os-release && echo \\\"\$VERSION_CODENAME\\\") stable\" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt-get update"
+add_docker_repository() {
+  local password
+  password="fer010486"
+  echo $password | sudo -S apt-get update
+  echo $password | sudo -S apt-get install ca-certificates curl
+  echo $password | sudo -S install -m 0755 -d /etc/apt/keyrings
+  echo $password | sudo -S curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  echo $password | sudo -S chmod a+r /etc/apt/keyrings/docker.asc
 
-  expect {
-      "password for" { send "$sudo_password\r"; exp_continue }
-      eof
-  }
-EOF
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+  echo $password | sudo -S apt-get update
 }
 
-
 # Function to check if Docker APT keyrings and repository are already set up
-check_docker_setup () {
+check_docker_setup() {
   # Check if the docker keyring file exists and is readable
   if [ -r /etc/apt/keyrings/docker.asc ]; then
     # Check if the Docker repository is in the sources list
     if grep -q "^deb .*/download.docker.com/linux/ubuntu" /etc/apt/sources.list.d/docker.list 2>/dev/null; then
-      return 0  # True
+      return 0 # True
     fi
   fi
-  return 1  # False
+  return 1 # False
 }
 
-install_docker_components () {
+install_docker_components() {
   # The command block to be executed by expect
-  expect << EOF
+  expect <<EOF
   set timeout -1
-  spawn /bin/bash -c "sudo apt install -y docker-ce-cli uidmap"
+  spawn /bin/bash -c "sudo apt install -y docker-ce-cli libqrencode4 uidmap tree qrencode xclip pass"
 
   expect {
       "password for" { send "$sudo_password\r"; exp_continue }
@@ -58,10 +58,9 @@ EOF
 }
 
 # Function to check whether Docker was installed and run hello-world container
-run_docker_hello_world () {
-  if command -v docker &> /dev/null
-  then
-    expect << EOF
+run_docker_hello_world() {
+  if command -v docker &>/dev/null; then
+    expect <<EOF
     set timeout -1
     spawn sudo docker run hello-world
 
