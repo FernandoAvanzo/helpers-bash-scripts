7f25f3f2a3e1108ff31b06e8dd3b2c59504fbeed
FernandoAvanzo <fernando.avanzo@gmail.com>
2024-10-06 11:56:37 -0300

    adjust the root password function fo use the 1password token

--- BashLib/src/helpers/root-password.sh
+++ BashLib/src/helpers/root-password.sh
@@ -2,8 +2,10 @@
 
 getRootPassword() {
   local psw
-  local vault
-  vault="$ROOT_VAULT_NAME"
-  psw="$(op item get "$vault" --format human-readable --fields password --reveal)"
+  local secret_iten
+  local vault_name
+  vault_name="Workstation_Automation"
+  secret_iten="$ROOT_SECRET_NAME"
+  psw="$(op item get "$secret_iten" --format human-readable --fields password --reveal --vault $vault_name)"
   echo "$psw"
 }
