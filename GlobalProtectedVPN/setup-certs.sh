53106a7c0cd03a61e1e1e0873bdac461042332e5
fernando.avanzo <fernando.avanzo@gmail.com>
2024-10-08 13:26:31 -0300

    create a scrip0t to copy the globla protected vpn certificates

---
+++ GlobalProtectedVPN/setup-certs.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+export HELPERS="$MY_CLI/BashLib/src/helpers"
+export GP_CERTS_FOLDER="$HOME/Applications/paloAlto/GlobalProtect/certificates"
+export SYSTEM_CERTS_FOLDER="/usr/local/share/ca-certificates"
+
+# shellcheck source=./../BashLib/src/helpers/root-password.sh
+source "$HELPERS"/root-password.sh
+
+password="$(getRootPassword)"
+
+copy_certificates() {
+  local file
+  for file in "$GP_CERTS_FOLDER"/*.crt; do
+    local filename
+    filename=$(basename -- "$file" .crt)
+    local new_folder="$SYSTEM_CERTS_FOLDER/$filename"
+    echo "$password" | sudo -S mkdir -p "$new_folder"
+    echo "$password" | sudo -S cp "$GP_CERTS_FOLDER/$filename.crt" "$new_folder"
+  done
+}
+
+
+check_and_change_permissions() {
+  local file
+  echo "$password" | sudo -S chmod 755 "$SYSTEM_CERTS_FOLDER"
+  for file in "$GP_CERTS_FOLDER"/*.crt; do
+    local filename
+    filename=$(basename -- "$file" .crt)
+    local target_folder="$SYSTEM_CERTS_FOLDER/$filename"
+    if [ -d "$target_folder" ]; then
+      echo "$password" | sudo -S chmod 644 "$target_folder/$filename.crt"
+    fi
+  done
+}
+
+copy_certificates
+check_and_change_permissions
+echo "$password" | sudo -S update-ca-certificates
