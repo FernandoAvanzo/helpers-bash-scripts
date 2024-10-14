d92fd4425c61db8762a6025c56d0afe7ad13e4c5
fernando.avanzo <fernando.avanzo@gmail.com>
2024-10-14 11:29:13 -0300

    create an integrity key generator script

--- BashLib/src/create_integrity_key.sh
+++ BashLib/src/create_integrity_key.sh
@@ -10,4 +10,4 @@ file=$1
 generate_sha1_checksum "$file"
 
 #Install Script
-# echo $(get-root-psw) | sudo -S ln -sf $MY_CLI/BashLib/src/create_integrity.sh /usr/bin/create_integrity
+# echo $(get-root-psw) | sudo -S ln -sf $MY_CLI/BashLib/src/create_integrity.sh /usr/bin/create_integrity_key
