d4a8d6aefa529afa1d04c674ddb768e94a613906
fernando.avanzo <fernando.avanzo@gmail.com>
2024-10-16 14:18:56 -0300

    adding sshpass install

--- Node/bin/node-utils.sh
+++ Node/bin/node-utils.sh
@@ -35,6 +35,7 @@ install_node_dependencies(){
   echo "$password" |  sudo -S apt install python-pip -y
   echo "$password" |  sudo -S apt install libusb-1.0-0 -y
   echo "$password" |  sudo -S apt install libusb-1.0-0-dev -y
+  echo "$password" |  sudo -S apt install sshpass -y
 }
 
 # shellcheck disable=SC1091,SC1090
