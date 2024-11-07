5209c0f7b2e59a8eb4264502dba2ee34eaedf0db
fernandoavanzo <fernando.avanzo@gmail.com>
2024-11-07 09:09:47 -0300

    improve scripts

--- BashLib/src/update_system.sh
+++ BashLib/src/update_system.sh
@@ -8,6 +8,10 @@ password="$(getRootPassword)"
 echo "$password" | sudo -S apt update -y
 echo "$password" | sudo -S apt upgrade -y
 echo "$password" | sudo -S apt autoremove -y
+echo "$password" | sudo -S apt clean -y
 echo "Refresh flatpak"
 flatpak update -y
 flatpak uninstall -y --unused
+
+#install Script
+#echo "$(get-root-psw)" | sudo -S ln -sf "$MY_CLI"/BashLib/src/update_system.sh /usr/bin/update_system
