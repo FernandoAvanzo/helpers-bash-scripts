24e344867eb66ba7723fe85e76ae4719f8bab857
fernandoavanzo <fernando.avanzo@gmail.com>
2024-11-07 09:50:52 -0300

    improve scripts

--- Docker/set-docker-default.sh
+++ Docker/set-docker-default.sh
@@ -6,3 +6,6 @@ export MY_DOCKER_LIBS="$MY_CLI/Docker/bin"
 source "$MY_DOCKER_LIBS/docker-utils.sh"
 
 check_and_link_docker_sock
+
+#install Script
+#echo "$(get-root-psw)" | sudo -S ln -sf "$MY_CLI"/Docker/set-docker-default.sh /usr/bin/set-docker-default
