b22bee59ca43ae98bfd689e96800aaad82c88124
fernando-avanzo <fernando.avanzo@gmail.com>
2024-12-19 11:10:32 -0300

    create a script to refresh the rclone token

--- rclone/bin/refresh_rclone_token.sh
+++ rclone/bin/refresh_rclone_token.sh
@@ -5,6 +5,8 @@ export RCLONE="$HOME/.Library/rclone"
 source "$RCLONE"/bin/manage_rclone_service.sh
 
 refresh_token_connection
-
+echo
+echo "Token refreshed successfully."
+exit
 
 #echo "$(get-root-psw)" | sudo -S ln -sf $HOME/.Library/rclone/bin/refresh_rclone_token.sh /usr/bin/refresh_rclone_token
