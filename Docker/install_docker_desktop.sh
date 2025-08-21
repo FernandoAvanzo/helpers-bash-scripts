23caf707b313166648f7c3a5b195c0ff89e25cf3
fernando.avanzo <fernando.avanzo@gmail.com>
2025-08-21 08:55:54 -0300

    Update docker desktop 4.44.3

--- Docker/install_docker_desktop.sh
+++ Docker/install_docker_desktop.sh
@@ -18,7 +18,7 @@ source "$HELPERS"/tool_check_script.sh
 
 
 password="$(getRootPassword)"
-url="https://desktop.docker.com/linux/main/amd64/202017/docker-desktop-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64&_gl=1*ojn5vs*_gcl_au*NDI0ODc3MjYyLjE3NTU1MTU5Njk.*_ga*MTUwMDgyNjEzNC4xNzMwMzE1MjY3*_ga_XJWPQMJYHQ*czE3NTU1MTU5NjUkbzE1JGcxJHQxNzU1NTE1OTk2JGoyOSRsMCRoMA.."
+url="https://desktop.docker.com/linux/main/amd64/202357/docker-desktop-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64&_gl=1*1fjcqzi*_gcl_au*NDI0ODc3MjYyLjE3NTU1MTU5Njk.*_ga*MTUwMDgyNjEzNC4xNzMwMzE1MjY3*_ga_XJWPQMJYHQ*czE3NTU3NzcyNjYkbzE2JGcxJHQxNzU1Nzc3MjY3JGo1OSRsMCRoMA.."
 dest_path="$HOME/Downloads/docker-desktop-amd64.deb"
 
 install_docker_desktop() {
