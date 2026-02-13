415fba5b7f780767c9c5ba27b0fb636da4811d01
fernando.avanzo <fernando.avanzo@gmail.com>
2026-02-13 18:39:03 -0300

    Comment out unused flatpak uninstall command in update_system.sh script.

--- BashLib/src/update_system.sh
+++ BashLib/src/update_system.sh
@@ -12,7 +12,7 @@ echo "$password" | sudo -S apt autoremove -y
 echo "$password" | sudo -S apt clean -y
 echo "Refresh flatpak"
 flatpak update -y
-flatpak uninstall -y --unused
+#flatpak uninstall -y --unused
 
 #install Script
 #echo "$(get-root-psw)" | sudo -S ln -sf "$MY_CLI"/BashLib/src/update_system.sh /usr/bin/update_system
